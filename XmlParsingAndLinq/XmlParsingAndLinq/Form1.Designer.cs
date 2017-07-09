aeef6fd429924718b13158c96b2d897bd9b3d58c
Jayce77 <xjaycex@hotmail.com>
2017-07-09 10:36:54 -0400

    Started using LINQ for XML

---
+++ XmlParsingAndLinq/XmlParsingAndLinq/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace XmlParsingAndLinq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.propertyGrid1.LineColor = System.Drawing.SystemColors.ControlDark;
+            this.propertyGrid1.Location = new System.Drawing.Point(4, 237);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(703, 237);
+            this.propertyGrid1.TabIndex = 0;
+            this.propertyGrid1.Click += new System.EventHandler(this.propertyGrid1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(686, 196);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 477);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ XmlParsingAndLinq/XmlParsingAndLinq/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlParsingAndLinq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadWords();
+        }
+
+        private void LoadWords()
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(@"C:\Users\That Guy\Documents\JMdict\JMdict_e.xml");
+
+            XDocument xdoc = XDocument.Load(@"C:\Users\That Guy\Documents\JMdict\JMdict_e.xml");
+            xdoc.Descendants("entry").Select( p => new {
+                id = p.Element("ent_seq").Value
+            }).ToList().ForEach(p => {
+                Console.WriteLine("Id: " + p.id);
+            });
+
+            //foreach (XmlNode node in document.DocumentElement)
+            //{
+                
+            //    Word word = new Word();
+            //    word.Id = int.Parse(node["ent_seq"].InnerText);
+            //    //word.Name = node.Attributes[1][0].InnerText;
+            //    if (node["sense"]["gloss"].InnerText != null)
+            //    {
+            //        word.Meaning = node["sense"]["gloss"].InnerText;
+            //    }
+            //    listBox1.Items.Add(word);
+            //    counter++;
+            //    if (counter > 15)
+            //    {
+            //        break;
+            //    }
+            //}
+        }
+
+        private void propertyGrid1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                propertyGrid1.SelectedObject = listBox1.SelectedItem;
+            }
+        }
+    }
+}
