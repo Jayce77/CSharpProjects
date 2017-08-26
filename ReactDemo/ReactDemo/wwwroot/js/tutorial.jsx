0bc24f8a30fcac3ac62a892d48dde2b55d82e083
Jayce77 <xjaycex@hotmail.com>
2017-08-26 18:31:16 -0400

    Added framework for CommentList and CommentForm components

--- ReactDemo/ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/ReactDemo/wwwroot/js/tutorial.jsx
@@ -1,8 +1,30 @@
-﻿var CommentBox = React.createClass({
+﻿var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList" >
+                This is a comment list
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                Comment form here
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
     render: function () {
         return (
             <div className="commentBox">
-                Hello, world! I am a comment box!
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
             </div>
         );
     }
