39317582fd27dbfb2b721f53f39d284a0e46a1d7
Jayce77 <xjaycex@hotmail.com>
2017-08-26 18:53:19 -0400

    Modified components to work with comment objects

--- ReactDemo/ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/ReactDemo/wwwroot/js/tutorial.jsx
@@ -1,4 +1,10 @@
-﻿var Comment = React.createClass({
+﻿var data = [
+    { id: 1, author: "Daniel Lo Nigro", text: "Hello ReactJS.NET World!" },
+    { id: 2, author: "Pete Hunt", text: "This is one comment" },
+    { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
+];
+
+var Comment = React.createClass({
     rawMarkup: function () {
         var md = new Remarkable();
         var rawMarkup = md.render(this.props.children.toString());
@@ -19,13 +25,15 @@
 
 var CommentList = React.createClass({
     render: function () {
-        return (
-            <div className="commentList" >
-                <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
-                <Comment author="Pete Hunt">This is one comment</Comment>
-                <Comment author="Jordan Walke">This is *another* comment</Comment>
-            </div>
-        );
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <div className="commentList" >
+                    <Comment author={ comment.author } key={ comment.id }>
+                        { comment.text }
+                    </Comment>
+                </div>
+            );
+        });
     }
 });
 
@@ -44,7 +52,7 @@ var CommentBox = React.createClass({
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
@@ -52,6 +60,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox data={this.props.data} />,
     document.getElementById('content')
 );
