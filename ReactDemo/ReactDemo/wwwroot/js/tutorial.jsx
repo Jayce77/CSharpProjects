622ea3ada08829e3d1c14b9a9c2db473953cf22f
Jayce77 <xjaycex@hotmail.com>
2017-08-27 18:19:46 -0400

    Fixed bugs

--- ReactDemo/ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/ReactDemo/wwwroot/js/tutorial.jsx
@@ -9,38 +9,27 @@ var CommentForm = React.createClass({
         return { author: '', text: '' };
     },
     handleAuthorChange: function(e) {
-
         this.setState({ author: e.target.value });
     },
     handleTextChange: function(e) {
         this.setState({ text: e.target.value });
     },
     handleSubmit: function(e) {
-        e.proventDefault();
+        e.preventDefault();
         var author = this.state.author.trim();
         var text = this.state.text.trim();
         if (!text || !author) {
             return;
         }
         //TODO: send request to the server
-        this.setState({ author: "", test: "" });
+        this.setState({ author: "", text: "" });
     },
-    render: function () {
+    render: function() {
         return (
-            <form className="commentsForm" onSubmit="this.handleSubmit">
-                <input
-                    type="text"
-                    placeholder="Your name"
-                    value={this.state.author}
-                    onChange={this.handleAuthorChange />
-                
-                <input
-                        type="text"
-                        placeholder="Say something..."
-                        value={this.state.text}
-                        onChange={this.handleTextChange} />
-                 
-                <input type="submit" value="Post" />
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+                <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+                <input type="submit" value="Post" />     
             </form>
         );
     }
