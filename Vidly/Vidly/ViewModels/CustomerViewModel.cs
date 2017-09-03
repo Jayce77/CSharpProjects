39b6650746a539ef2a0dfaf75d6f5615afd709d5
Jayce77 <xjaycex@hotmail.com>
2017-09-03 12:00:31 -0400

    Got rid of RandomMovie and Created Customer and Movie ViewModels

--- Vidly/Vidly/ViewModels/CustomerViewModel.cs
+++ Vidly/Vidly/ViewModels/CustomerViewModel.cs
@@ -6,9 +6,9 @@ using Vidly.Models;
 
 namespace Vidly.ViewModels
 {
-    public class RandomMovieViewModel
+    public class CustomerViewModel
     {
-        public Movie Movie { get; set; }
         public List<Customer> Customers { get; set; }
+
     }
 }
