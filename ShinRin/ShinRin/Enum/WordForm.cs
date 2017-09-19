2a1e734805ec1e81147450377fd94e5ef8a5499f
Jayce77 <xjaycex@hotmail.com>
2017-09-18 21:58:34 -0400

    Added Enums for word forms

---
+++ ShinRin/ShinRin/Enum/WordForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShinRin.Enum
+{
+    public enum WordForm
+    {
+        [Display(Name="I-Adjentive")]
+        IADJFORM,
+
+        [Display(Name = "Na-Adjentive")]
+        NAADJFORM,
+
+        [Display(Name = "Noun")]
+        NOUNFORM,
+
+        [Display(Name = "Other")]
+        OTHERFORM,
+
+        [Display(Name = "Verb")]
+        VERBFORM
+    }
+}
